c7e3867c311159e0f82bec2fa3f5dd4b17582178
murikadan <yassermurikadan@live.com>
2018-06-02 12:08:41 +0530

    tidy up exception handling a bit

--- main.py
+++ main.py
@@ -2,20 +2,29 @@
 Main script
 """
 
+import sys
 import quick_sort
 
 def main():
     "main function!!"
-    infile = open('input.txt', 'r')
-    numbers = infile.readlines()
-    infile.close()
+    try:
+        infile = open('input.txt', 'r')
+        numbers = infile.readlines()
+        infile.close()
+    except:
+        print('An error occurred while trying to read the file', sys.exc_info()[0])
+        sys.exit(1)
     input = []
     for num in numbers:
         input.append(int(num.strip()))
     output=quick_sort.qsort(input)
-    outfile = open('output.txt','w')
-    for num in output:
-        outfile.write("%s\n" % num)
+    try:
+        outfile = open('output.txt','w')
+        for num in output:
+            outfile.write("%s\n" % num)
+    except IOError:
+        print('An error occurred while trying to write to file')
+        sys.exit(1)
 
 
 if __name__ == "__main__":
